8d73ea32973e9d8d5107a0a0cb9cdd68ca810d22
BelyashMatvey <belyakov06mat@mail.ru>
2024-09-19 21:52:52 +0300

    Initial commit

---
+++ HW2_Belyakov/calculator.py
@@ -0,0 +1,32 @@
+def adding(a, b):
+    pass
+
+
+def diff(a, b):
+    pass
+
+
+def mult(a, b):
+    pass
+
+
+def division(a, b):
+    pass
+
+
+def main():
+    expression = input().split()
+    a = int(expression[0])
+    oper = expression[1]
+    b = int(expression[2])
+    if oper == '+':
+        print(adding(a, b))
+    if oper == '-':
+        print(diff(a, b))
+    if oper == '*':
+        print(mult(a, b))
+    else:
+        print(division(a, b))
+
+
+main()
